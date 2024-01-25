1fe734dba955a2555e8ed2e5c64c9e95c0f0f926
zoetanghk852 <zoetang852@gmail.com>
2024-01-25 17:02:31 +0800

    pnpm

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from "./component/footer";
 import Home from "./page/Home";
 import Tour from "./page/tourpage/Tour";
 function App() {
+  document.title = "Lobe";
   return (
     <div className="App">
       <Navigation />
